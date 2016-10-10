f33d93369425cbd85cc5cebe37fa2c90fe8b0bdb
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2016-10-10 22:16:06 +1000

    Make all ping tests pass

--- pings (backend)/server.rb
+++ pings (backend)/server.rb
@@ -4,8 +4,13 @@ require 'date'
 
 devices = {}
 
-post ('/clear_data') { devices.clear }
-get ('/devices') { devices.to_json }
+post '/clear_data' do
+  devices.clear
+end
+
+get '/devices' do
+  devices.keys.to_json
+end
 
 post '/:device_id/:epoch_time' do |device_id, epoch_time|
   devices[device_id] ||= []
@@ -18,26 +23,30 @@ end
 
 get '/all/:from/:to' do |from, to|
   from = gracefully_parse_date(from)
-  to = gracefully_parse_date(to)
+  to   = gracefully_parse_date(to)
 
   devices.inject({}) { |h, (k, v)| h[k] = v.select{ |d| timestamp_between?(d, from, to)}; h }.to_json
 end
 
 get '/:device_id/:from/:to' do |device_id, from, to|
   from = gracefully_parse_date(from)
-  to = gracefully_parse_date(to)
+  to   = gracefully_parse_date(to)
 
-  devices[device_id].select do |date|
-    timestamp_between?(date, from, to)
+  if devices[device_id]
+    devices[device_id].select do |date|
+      timestamp_between?(date, from, to)
+    end
+  else
+    []
   end.to_json
 end
 
 def timestamp_between?(timestamp, from, to)
-  (from ... to).cover? timestamp
+  (from...to).cover? timestamp
 end
 
 def timestamp_to_date(timestamp)
- parse_timestamp(timestamp).to_date
+  parse_timestamp(timestamp).to_date
 end
 
 def parse_timestamp(timestamp)
@@ -47,4 +56,3 @@ end
 def gracefully_parse_date(date)
   date.include?('-') ? Date.parse(date) : parse_timestamp(date)
 end
-
